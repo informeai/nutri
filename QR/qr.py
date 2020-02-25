c202d024f2adefac4b9e5f0d4c1c1e2ece767924
Wellington Gadelha <wellingtongadelha@Informeai.local>
2020-02-25 16:57:56 -0300

    teste de leitura de qr

--- QR/qr.py
+++ QR/qr.py
@@ -1,9 +1,12 @@
 import pyqrcode
 
-url = 'URL DO SERVIDOR MAI A ROTA'
-product = 'NOME DO PRODUTO CADASTRADO NO BANCO DE DADOS'
+url = 'nutri-informeai.herokuapp.com/products/'
+product = 'tortaconfeitada'
 
 # Criar Qrcodes
 def create_qr(url,product):
     qr = pyqrcode.create(f'{url}{product}')
     qr.png(f'{product}.png',scale=6)
+
+if __name__ == '__main__':
+    create_qr(url,product)
